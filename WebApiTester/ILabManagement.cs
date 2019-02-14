5340d921c80010ef0a72edd7313352720a34009d
Jonathan Allen <jallen@snow.edu>
2019-02-14 16:28:34 -0700

    Play with refit library

---
+++ WebApiTester/ILabManagement.cs
@@ -0,0 +1,33 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTester
+{
+    public class ReportItem
+    {
+        public int Id { get; set; }
+        public string HostName { get; set; }
+        public string SerialNumber { get; set; }
+        public string MacNumber { get; set; }
+        public string ImageVersionContent { get; set; }
+        public DateTime ReportTime { get; set; }
+    }
+
+    public interface ILabManagement
+    {
+        [Get("/api/ReportItems")]
+        Task<IEnumerable<ReportItem>> GetReportItemsAsync();
+
+        [Post("/api/ReportItems")]
+        Task<ReportItem> AddReportItemAsync(ReportItem reportItem);
+
+        [Get("/api/ReportItems/{id}")]
+        Task<ReportItem> GetReportItemAsync(int id);
+
+        [Put("/api/ReportItems/{id}")]
+        Task<bool> UpdateReportItemAsync(int id, ReportItem reportItem);
+    }
+}
--- WebApiTester/Program.cs
+++ WebApiTester/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
 
 namespace WebApiTester
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var client = RestService.For<ILabManagement>("http://smartdeploy:5000");
+
+            foreach(var item in await client.GetReportItemsAsync())
+            {
+                Console.WriteLine($"{item.HostName} @ {item.ReportTime}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Now put in a name and we can add it");
+            var hostName = Console.ReadLine();
+
+            var newItem = new ReportItem { HostName = hostName };
+
+            Console.WriteLine($"Adding new {newItem.HostName} item...");
+            var committedItem = await client.AddReportItemAsync(newItem);
+
+            Console.WriteLine($"ID for new {newItem.HostName} item: {committedItem.Id}");
+
+
+            Console.WriteLine("Enter an ID of an item you'd like to retrieve");
+            var requestedId = int.Parse(Console.ReadLine());
+            var requestedItem = await client.GetReportItemAsync(requestedId);
+
+            Console.WriteLine($"You requested {requestedItem.Id} @ {requestedItem.ReportTime} for {requestedItem.HostName}");
+
+            Console.WriteLine("Enter a new value for the image version content");
+            var newImageVersionContent = Console.ReadLine();
+
+            requestedItem.ImageVersionContent = newImageVersionContent;
+            var success = await client.UpdateReportItemAsync(requestedId, requestedItem);
+            Console.WriteLine($"Update success? {success}");
+
+
+            Console.WriteLine("All done.");
+            Console.ReadLine();
         }
     }
 }
